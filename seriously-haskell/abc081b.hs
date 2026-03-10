f5211cc41b91c1dc96ed8b6c3fe8af1c8ac75877
timdaik <hirokix0608@gmail.com>
2026-03-11 00:19:54 +0900

    fix: 模範解答をほぼコピペ

--- seriously-haskell/abc081b.hs
+++ seriously-haskell/abc081b.hs
@@ -1,17 +1,14 @@
 main :: IO ()
 
--- 問題を読み間違えていた
--- 正しくはリスト全体が偶数である場合に全ての要素を半分にしてカウントするもの
-
--- リストの要素を2で割ったものを返してカウントを+1する
-devideBy2AndCount :: [Int] -> Int
-devideBy2AndCount [] = 0
-devideBy2AndCount (x:xs)
-    | even x = 1 + devideBy2AndCount ((x `div` 2):xs)
-    | otherwise = devideBy2AndCount xs
+countTwo :: Int -> Int
+countTwo x0 = inner 0 x0
+    where
+        inner count x
+            | x `mod` 2 == 1 = count
+            | otherwise = inner (count + 1) (x `div` 2)
 
 main = do
-    n <- readLn :: IO Int
+    _ <- readLn :: IO Int
     nums <- map read . words <$> getLine :: IO [Int]
-    -- リスト内法表記で条件に合うものを抽出する
-    print $ devideBy2AndCount nums
+    let counts = map countTwo nums
+    print $ minimum counts
