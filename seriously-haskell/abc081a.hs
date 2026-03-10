a449c7deee06c58b985611d4f0b8631c6be9c84d
timdaik <hirokix0608@gmail.com>
2026-03-10 20:34:21 +0900

    fix: 構文エラーの修正 - パターンマッチは空状態・初期値の定義を忘れないようにする - 型の不一致に気を付ける

--- seriously-haskell/abc081a.hs
+++ seriously-haskell/abc081a.hs
@@ -1,13 +1,11 @@
 main :: IO ()
 
 countMarbles :: String -> Int
-countMarbles "1" = 1
-countMarbles "0" = 0
-countMarbles (x:xs) = (countMarbles x) + (countMarbles xs)
--- 空リストの場合が定義されていない
--- x は Char なので型の不一致が起きている
+countMarbles "" = 0
+countMarbles (x:xs)
+    | x == '1' = 1 + countMarbles xs
+    | otherwise = 0 + countMarbles xs
 
 main = do
     x <- getLine
-    -- putStrLn は String を受け取るが、実際に渡されているのは Int
-    putStrLn $ countMarbles x
+    print $ countMarbles x
