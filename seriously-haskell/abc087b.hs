dafb715d7175c069bcb1b29d450af55297fc9257
timdaik <hirokix0608@gmail.com>
2026-03-11 06:21:27 +0900

    fix: 変数名を修正 - ターゲットにマッチしたリストのペアという意味が分かりやすいようにした

--- seriously-haskell/abc087b.hs
+++ seriously-haskell/abc087b.hs
@@ -5,5 +5,5 @@ main = do
     b <- readLn
     c <- readLn
     target <- readLn
-    let result = [(x, y, z) | x<-[0..a], y<-[0..b], z<-[0..c], (500*x + 100*y + 50*z) == target]
-    print $ length result
+    let matchedPairs = [(x, y, z) | x<-[0..a], y<-[0..b], z<-[0..c], (500*x + 100*y + 50*z) == target]
+    print $ length matchedPairs
