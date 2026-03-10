d7bf0a88429b3ff554f81aa8d9b46bd1357e240c
timdaik <hirokix0608@gmail.com>
2026-03-10 19:13:04 +0900

    fix: スペースごとに数値入力を受け取る方法を覚えた

--- seriously-haskell/practive-a.hs
+++ seriously-haskell/practive-a.hs
@@ -3,7 +3,7 @@ main :: IO ()
 main = do
     -- Input
     a <- readLn :: IO Int
-    [b, c] <- readLn :: IO [Int]
+    [b, c] <- map read . words <$> getLine :: IO [Int]
     s <- getLine
     -- Output
     putStrLn $ show (a + b + c) ++ " " ++ s
