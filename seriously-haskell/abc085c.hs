ea8aa2a6f32cc551a9d7c6a2450ac81aae500e7d
timdaik <hirokix0608@gmail.com>
2026-03-13 00:11:48 +0900

    fix: 条件文を修正 - 問題文を読み間違えていた - 表示する札束の並びが1000と10000で逆

--- seriously-haskell/abc085c.hs
+++ seriously-haskell/abc085c.hs
@@ -6,7 +6,7 @@ solve 0 _ = [-1, -1, -1]
 solve _ 0 = [-1, -1, -1]
 -- NOTE: head とリスト連結で初期値を設定できる by AI
 solve n y = head $
-    [[na, nb, nc] | na <- [0..n], nb <- [0..n - na], let nc = (n - na - nb), 1000*na + 5000*nb + 10000*nc == y]
+    [[na, nb, nc] | na <- [0..n], nb <- [0..n - na], let nc = (n - na - nb), 10000*na + 5000*nb + 1000*nc == y]
     ++ [[-1, -1, -1]]
 
 main = do
