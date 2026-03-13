4421dab305cdbae7e9c89f90ef6b01a6b2768c67
timdaik <hirokix0608@gmail.com>
2026-03-13 15:03:18 +0900

    add: 一度間違ったパターンを選ぶとミスる

---
+++ seriously-haskell/abc049c.hs
@@ -0,0 +1,21 @@
+main :: IO ()
+
+include :: String -> String -> Bool
+include _ [] = True
+include (x:xs) (y:ys)
+    | x == y = include xs ys
+    | otherwise = False
+
+solve :: String -> String
+solve [] = "YES"
+solve xs
+    | include xs "dream" || include xs "erase" = solve $ drop 5 xs
+    | include xs "eraser" = solve $ drop 6 xs
+    | include xs "dreamer" = solve $ drop 7 xs
+    | otherwise = "NO"
+
+
+main = do
+    s <- getLine
+    let list = solve s
+    putStrLn list
