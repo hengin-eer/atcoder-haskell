12eeebf942e255bf860cf4cab28d96913a2d89ec
timdaik <hirokix0608@gmail.com>
2026-03-13 15:19:03 +0900

    fix: パターンマッチのタイポ修正

--- seriously-haskell/abc049c.hs
+++ seriously-haskell/abc049c.hs
@@ -11,7 +11,7 @@ solve :: String -> String
 solve [] = "YES"
 solve xs
     | include xs "maerd" || include xs "esare" = solve $ drop 5 xs
-    | include xs "reraser" = solve $ drop 6 xs
+    | include xs "resare" = solve $ drop 6 xs
     | include xs "remaerd" = solve $ drop 7 xs
     | otherwise = "NO"
 
