3a4a977c26f7fe80a07c1375fd5dac449d7ed224
timdaik <hirokix0608@gmail.com>
2026-03-13 18:15:00 +0900

    fix: リストの表記法修正とmodの引数追加

--- seriously-haskell/abc086c.hs
+++ seriously-haskell/abc086c.hs
@@ -4,17 +4,16 @@ main :: IO ()
 ints = map read . words <$> getLine
 
 reachable :: [Int] -> [Int] -> Bool
--- パリティについての条件判定
 reachable _ [] = True
-reachable (ti:xi:yi) (tj:xj:yj)
-    | dx + dy <= dt && mod dt == mod (dx + dy) = True
+-- NOTE: `(x:y:z)`では要素とリストの結合を表しているため`z`がリスト扱いになってしまう
+-- `[x,y,z]`のように要素であることを明示する
+reachable [ti, xi, yi] [tj, xj, yj]
+    | dx + dy <= dt && dt `mod` 2 == (dx + dy) `mod` 2 = True
     | otherwise = False
         where
             dx = abs (xj - xi)
             dy = abs (yj - yi)
             dt = tj - ti
-
--- 移動距離マスの和が時間辺りの最大移動距離を超えないか判定
 
 solve :: [[Int]] -> String
 solve [x] = "Yes"
