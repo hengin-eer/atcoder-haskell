ce5dd2e05bc4fa6aab98639e80588ba26759c4ec
timdaik <hirokix0608@gmail.com>
2026-03-15 00:33:27 +0900

    fix: 計算量を減らしたつもりだけどTLEだった

--- abc/449/c.hs
+++ abc/449/c.hs
@@ -6,5 +6,6 @@ ints = map read . words <$> getLine
 main = do
     [n, l, r] <- ints
     s <- getLine
-    let pairs = [[i, j] | i <- [0 .. n - 1], j <- [0 .. n - 1], i <= j && (s !! i == s !! j) && (l <= j - i && j - i <= r)]
+    -- TODO: `n`を計算して時間計算量を減らす
+    let pairs = [[i, j] | i <- [0 .. n - 1 - l], j <- [l .. n - 1], i <= j && (s !! i == s !! j) && (l <= j - i && j - i <= r)]
     print $ length pairs
