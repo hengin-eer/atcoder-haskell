6479ef893a5fd70e614d5d8caa932d1e37a2df91
timdaik <hirokix0608@gmail.com>
2026-03-21 19:37:44 +0900

    fix: warningに沿ってリストの連結方法を変更 以前は [x] ++ ...だったが、(x) : ...に変更

--- abc/449/b.hs
+++ abc/449/b.hs
@@ -1,6 +1,6 @@
 import Control.Monad (replicateM)
-main :: IO ()
 
+main :: IO ()
 ints :: IO [Int]
 ints = map read . words <$> getLine
 
@@ -9,10 +9,10 @@ solve _ _ [] = []
 solve 0 _ _ = []
 solve _ 0 _ = []
 solve h w ([qtype, point] : xs)
-    | qtype == 1 = [point * w] ++ solve (h - point) w xs
-    | otherwise = [point * h] ++ solve h (w - point) xs
+  | qtype == 1 = (point * w) : solve (h - point) w xs
+  | otherwise = (point * h) : solve h (w - point) xs
 
 main = do
-    [h, w, q] <- map read . words <$> getLine :: IO [Int]
-    queries <- replicateM q ints
-    putStrLn . unlines . map show $ solve h w queries
+  [h, w, q] <- map read . words <$> getLine :: IO [Int]
+  queries <- replicateM q ints
+  putStrLn . unlines . map show $ solve h w queries
